4948f43a2944b0d605f61dbd2a4de225bad2747e
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-10-10 15:27:12 -1100

    created mvc folder structure and models

---
+++ BlogProject.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BlogProject.Tests
+{
+    public class BlogProjectTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
