91c3aff619c933cef8605bc4860b6289231e2844
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-10-14 06:40:14 -1100

    added mvc startup info

--- BlogProject/Startup.cs
+++ BlogProject/Startup.cs
@@ -15,6 +15,7 @@ namespace BlogProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +25,19 @@ namespace BlogProject
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Product/Error");
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
