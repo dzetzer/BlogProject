446bc004faee1072f716ecbc91aec7fe0a66c971
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-10-14 06:32:09 -1100

    added BlogContext info

--- BlogProject/BlogContext.cs
+++ BlogProject/BlogContext.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogProject
 {
-    public class BlogContext
+    public class BlogContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=BlogProject;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData();
+        }
     }
 }
