e6c12002e9f82f13e9f5b80180a344aa63b69f4d
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-10-16 15:27:20 -1100

    worked on create form and postcontrollers

--- BlogProject/Controllers/PostController.cs
+++ BlogProject/Controllers/PostController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogProject.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.Controllers
 {
     public class PostController : Controller
     {
-        public ViewResult Index()
+        PostRepository postRepo;
+        CategoryRepository categoryRepo;
+
+        public PostController(PostRepository postRepo, CategoryRepository categoryRepo)
         {
+            this.postRepo = postRepo;
+            this.categoryRepo = categoryRepo;
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            ViewBag.Categories = categoryRepo.GetAll();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Models.Post post)
+        {
+            post.PublishDate = DateTime.Now;
+            postRepo.Create(post);
+            return RedirectToAction("Browse/Browse", new { });
         }
     }
 }
